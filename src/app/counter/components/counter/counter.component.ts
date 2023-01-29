a3aeb8d455e47f69baa3be4586315d9f053edf27
Hernan Valls <hernan.r.valls@gmail.com>
2023-01-29 14:22:04 +0100

    Finalizada seccion 5

--- src/app/counter/components/counter/counter.component.ts
+++ src/app/counter/components/counter/counter.component.ts
@@ -2,19 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template: `<h3>Counter: {{ counter }}</h3>
+  template: `
+    <h3>Counter: {{ counter }}</h3>
 
+    <button (click)="increaseBy(+1)">+1</button>
+    <button (click)="resetCounter()">Reset</button>
     <button (click)="increaseBy(-1)">-1</button>
-    <button (click)="resetValue()">Reset</button>
-    <button (click)="increaseBy(+1)">+1</button>`,
+
+  `
 })
-export class CounterComponent {
+export class CounterComponent  {
   public counter: number = 10;
 
-  increaseBy(value: number): void {
+  increaseBy( value: number ):void {
     this.counter += value;
   }
-  resetValue(): void {
+
+  resetCounter() {
     this.counter = 10;
   }
 }
--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -3,26 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css'],
+  styleUrls: ['./hero.component.css']
 })
 export class HeroComponent {
-  public name: string = 'ironman';
-  public age: number = 45;
 
-  get capitalzedName(): string {
+  public name: string = 'ironman';
+  public age:  number = 45;
+
+  get capitalizedName():string {
     return this.name.toUpperCase();
   }
-  getHeroDescription(): string {
-    return `${this.name}-${this.age}`;
+
+  getHeroDescription():string {
+    return `${ this.name } - ${ this.age }`;
   }
-  changeHero(): void {
-    this.name = 'Superman';
+
+  changeHero():void {
+    this.name = 'Spiderman'
   }
-  changeAge(): void {
+
+  changeAge():void {
     this.age = 25;
   }
-  resetForm(): void {
+
+  resetForm():void {
     this.name = 'ironman';
     this.age = 45;
+
+    // document.querySelectorAll('h1')!.forEach( element => {
+    //   element.innerHTML = '<h1>Desde Angular</h1>';
+    // });
   }
+
 }
